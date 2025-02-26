33af3311e3d00b539b0f83eee726832868f134fc
Xizheng Yu <yuxizheng@outlook.com>
2025-02-26 12:08:08 -0500

    add better job submission file..

---
+++ submit.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+# parameters: model_name, ip_bands, cl_lambda, cls_input_size, gpus, batch_size, bypass, time
+
+# chresmax_v2
+# ./submit_job.sh chresmax_v2 3 0 9216 8 32 False "120:00:00"
+# ./submit_job.sh chresmax_v2 3 0 18432 8 32 True "120:00:00"
+./submit_job.sh chresmax_v2 3 1 9216 8 32 False "120:00:00"
+./submit_job.sh chresmax_v2 3 1 18432 8 32 True "120:00:00"
+
+# resmax_v2
+# ./submit_job.sh resmax_v2 1 0 6400 8 128 False "48:00:00"
+# ./submit_job.sh resmax_v2 1 0 15616 8 128 True "72:00:00"
+# ./submit_job.sh resmax_v2 3 0 9216 8 64 False "48:00:00"
+# ./submit_job.sh resmax_v2 3 0 18432 8 64 True "72:00:00"
